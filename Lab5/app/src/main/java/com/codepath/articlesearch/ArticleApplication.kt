61b903602f446435fa11d5ca1a6a18b9105e009e
Zaire <150380756+rzVance@users.noreply.github.com>
2024-12-09 16:23:45 -0500

    Delete Lab5 directory

--- Lab5/app/src/main/java/com/codepath/articlesearch/ArticleApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.codepath.articlesearch
-
-import android.app.Application
-
-class ArticleApplication : Application() {
-    val db by lazy { AppDatabase.getInstance(this) }
-}
--- Lab5/app/src/main/java/com/codepath/articlesearch/DisplayArticle.kt
+++
@@ -1,8 +0,0 @@
-package com.codepath.articlesearch
-
-data class DisplayArticle(
-    val headline: String?,
-    val abstract: String?,
-    val byline: String?,
-    val mediaImageUrl: String?
-) : java.io.Serializable
